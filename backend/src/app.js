2d221bc4289b80409a1d3b21f7f298a89b87e1fa
Fernando Jorge <fernando.junior@ccc.ufcg.edu.br>
2020-03-28 08:59:18 -0300

    Finalizando projeto

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const { errors } = require('celebrate')
 const routes = require('./routes')
 
 
@@ -8,8 +9,9 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(routes)
+app.use(errors())
 
-app.listen(3333)
+module.exports = app
 
 
 
@@ -17,7 +19,7 @@ app.listen(3333)
 
 /**
  * Rota é o caminho completo. localhost3000/users
- * Recurso é o que desejo acessar. /users * 
+ * Recurso é o que desejo acessar. /users *
  */
 /**
  * Métodos HTTP:
@@ -25,7 +27,7 @@ app.listen(3333)
  * POST: Criar uma informação no back.
  * PUT: Alterar uma informação no back.
  * DELETE: Deletar uma informação no back.
- *  
+ *
 */
 
 /**
@@ -38,10 +40,36 @@ app.listen(3333)
  * Bancos de Dados:
  * SQL: MySQL, SQlite, PostgreSQL, Oracle, Microsoft SQL Server
  * NoSQL: MongoDB, CouchDB, etc
- * 
+ *
  * Formas de se relacionar com o banco de dados:
  * Drivers: Comunicação do tipo : SELECT * FROM users.
  * Query Builder: Comunicação do tipo: table('users').select('*').where()
  */
 
+/**
+ * Teste de Integração: São testes que vão testar o fluxo inteiro da aplicação. Testa por completo uma funcionalidade do programa.
+ * Teste Unitario: São teste que validam partes muito especificas do código/ projeto e que foram criados pelo próprio desenvolvedor.
+ * 
+ */
 
+ /**
+  * Deploy: 
+  * Backend : 
+  * Heroku para aplicação de pequeno porte
+  * DigitalOcean para aplicações maiores/comerciais com um preço razoavel.
+  * Amazom, Google  para aplicações muito robustas.
+  * 
+  * Frontend:
+  * Netlify pazra aplicações de pequeno porte(expecifico front)
+  * 
+  * Mobile:
+  * Publicação da aplicações na loja
+  * 
+  */
+
+  /**
+   * Estudos futuros:
+   * Padrões de código: ESlint, Prettier...
+   * Autenticação JWT
+   * Styled Componnents
+   */
